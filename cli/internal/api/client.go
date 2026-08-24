07661ce8b3cdb23838a442820356fc0991ab7b81
17673651504 <397996291@qq.com>
2026-08-24 19:45:56 +0800

    refactor(api): 合并 sign.go 到 client.go，删除 sign.go

--- cli/internal/api/client.go
+++ cli/internal/api/client.go
@@ -21,7 +21,25 @@ import (
 const (
 	successCode        = "0000"
 	TunnelNotFoundCode = "10002"
+
+	headerXAPIKey         = "X-API-Key"
+	headerContentType     = "content-type"
+	headerApplicationJson = "application/json"
 )
+
+var errMissingAPIKey = errors.New("missing api key")
+
+func signRequest(cred *auth.Credential, req *http.Request) error {
+	if cred == nil || cred.APIKey == "" {
+		return errMissingAPIKey
+	}
+
+	req.Header.Set(headerXAPIKey, cred.APIKey)
+	if req.Header.Get(headerContentType) == "" {
+		req.Header.Set(headerContentType, headerApplicationJson)
+	}
+	return nil
+}
 
 type apiError struct {
 	Code    string
